ee0cf07162e6d269857b55b0540bfea92227d4b2
jeremyHarrell <hillvulf@gmail.com>
2023-01-30 15:22:09 -0600

    meal maker complete

---
+++ mealMakerObjectProject.js
@@ -0,0 +1,57 @@
+// Meal Maker
+// A restaurant has hired you to create a function for their website that allows them to set a meal and price each morning for Today’s Special. Use your knowledge of getters and setters to make sure anyone who uses the new function can generate a meal and a price for Today’s Special without any embarrassing errors!
+
+let num = Math.floor(Math.random() * 3)
+const menu = {
+  _meal: '',
+  _price: 0,
+  set meal(mealToCheck) {
+    if (typeof mealToCheck === 'string'){
+      return menu._meal = mealToCheck
+    }
+  },
+  set price(priceToCheck) {
+    if (typeof priceToCheck === 'number'){
+      return menu._price = priceToCheck
+    }
+  },
+  get todaysSpecial(){
+    if (this._meal && this._price){
+      return (`Today's Special is ${this._meal} for $${this._price}!`)
+    } else {
+      return "Meal or price was not set correctly"
+    }
+  }
+}
+
+menu.meal = 'Beef'
+menu.price = 15
+
+console.log(menu.todaysSpecial)
+// let num = Math.floor(Math.random() * 3)
+// const menu = {
+//   _meal: '',
+//   _price: 0,
+//   set meal(mealToCheck) {
+//     if (typeof mealToCheck === 'object'){
+//       return menu._meal = mealToCheck
+//     }
+//   },
+//   set price(priceToCheck) {
+//     if (typeof priceToCheck === 'object'){
+//       return menu._price = priceToCheck
+//     }
+//   },
+//   get todaysSpecial(){
+//     if (this._meal && this._price){
+//       return (`Today's Special is ${menu._meal[num]} for $${this._price[num]}!`)
+//     } else {
+//       return "Meal or price was not set correctly"
+//     }
+//   }
+// }
+
+// menu.meal = ['Beef', 'Chicken', 'Squid']
+// menu.price = [15, 30, 50]
+
+// console.log(menu.todaysSpecial)
