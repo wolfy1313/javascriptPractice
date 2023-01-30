07ee7933d08cbb1840e6f509533346ff46bdd468
jeremyHarrell <hillvulf@gmail.com>
2023-01-30 10:54:24 -0600

    advanced Objects 2

--- advancedObjects.js
+++ advancedObjects.js
@@ -1,9 +1,18 @@
-const robot = {
-  model: '1E78V2',
-  energyLevel: 100,
-  provideInfo(){
-    return (`I am ${this.model} and my current energy level is ${this.energyLevel}.`)
-  }
-};
+// const robot = {
+//   model: '1E78V2',
+//   energyLevel: 100,
+//   provideInfo(){
+//     return (`I am ${this.model} and my current energy level is ${this.energyLevel}.`)
+//   }
+// };
 
-console.log(robot.provideInfo())
+// console.log(robot.provideInfo())
+
+const robot = {
+  energyLevel: 100,
+  checkEnergy() {
+    console.log(`Energy is currently at ${this.energyLevel}%.`)
+  }
+}
+
+robot.checkEnergy();
