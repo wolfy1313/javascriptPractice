444107834ce25bb987c9e673a04eb26b9b24bdc2
jeremyHarrell <hillvulf@gmail.com>
2023-01-30 14:31:10 -0600

    advanced Objects 4

--- advancedObjects.js
+++ advancedObjects.js
@@ -40,25 +40,51 @@
 // robot.numOfSensors = 100
 // console.log(robot.numOfSensors)
 
+// const robot = {
+//   _model: '1E78V2',
+//   _energyLevel: 100,
+//   _numOfSensors: 15,
+//   get numOfSensors(){
+//     if(typeof this._numOfSensors === 'number'){
+//       return this._numOfSensors;
+//     } else {
+//       return 'Sensors are currently down.'
+//     }
+//   },
+//   set numOfSensors(num){
+//     if (typeof num === "number" && num >=0){
+//       this._numOfSensors = num
+//     } else {
+//       console.log("Pass in a number that is greater than or equal to 0")
+//     }
+//   }
+// };
+
+// robot.numOfSensors = 100
+// console.log(robot.numOfSensors)
+
 const robot = {
-  _model: '1E78V2',
-  _energyLevel: 100,
-  _numOfSensors: 15,
-  get numOfSensors(){
-    if(typeof this._numOfSensors === 'number'){
-      return this._numOfSensors;
-    } else {
-      return 'Sensors are currently down.'
-    }
-  },
-  set numOfSensors(num){
-    if (typeof num === "number" && num >=0){
-      this._numOfSensors = num
-    } else {
-      console.log("Pass in a number that is greater than or equal to 0")
-    }
-  }
+	model: 'SAL-1000',
+  mobile: true,
+  sentient: false,
+  armor: 'Steel-plated',
+  energyLevel: 75
 };
 
-robot.numOfSensors = 100
-console.log(robot.numOfSensors)
+// What is missing in the following method call?
+const robotKeys = Object.keys(robot);
+
+console.log(robotKeys);
+
+// Declare robotEntries below this line:
+const robotEntries = Object.entries(robot)
+
+console.log(robotEntries);
+
+// Declare newRobot below this line:
+const newRobot = Object.assign({
+  laserBlaster: true,
+  voiceRecognition: true
+}, robot)
+
+console.log(newRobot);
