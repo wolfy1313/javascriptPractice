8667cc76817803b01cc7184cb99ef0c90bf3f334
jeremyHarrell <hillvulf@gmail.com>
2023-01-30 11:54:40 -0600

    advanced Objects 3

--- advancedObjects.js
+++ advancedObjects.js
@@ -8,11 +8,34 @@
 
 // console.log(robot.provideInfo())
 
-const robot = {
-  energyLevel: 100,
-  checkEnergy() {
-    console.log(`Energy is currently at ${this.energyLevel}%.`)
-  }
-}
+// const robot = {
+//   energyLevel: 100,
+//   checkEnergy() {
+//     console.log(`Energy is currently at ${this.energyLevel}%.`)
+//   }
+// }
 
-robot.checkEnergy();
+// robot.checkEnergy();
+
+const robot = {
+  _model: '1E78V2',
+  _energyLevel: 100,
+  _numOfSensors: 15,
+  get numOfSensors(){
+    if(typeof this._numOfSensors === 'number'){
+      return this._numOfSensors;
+    } else {
+      return 'Sensors are currently down.'
+    }
+  },
+  set numOfSensors(num){
+    if (typeof num === "number" && num >=0){
+      this._numOfSensors = num
+    } else {
+      console.log("Pass in a number that is greater than or equal to 0")
+    }
+  }
+};
+
+robot.numOfSensors = 100
+console.log(robot.numOfSensors)
